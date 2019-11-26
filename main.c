79c502b13f66ba8aad8e34f070c237870258e09e
SoohyeonHong <tngus4411@naver.com>
2019-11-26 17:26:20 +0900

    1

---
+++ main.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+
+#01
+int main(void){
+	
+struct student {
+	int ID;
+	char name[10];
+	double grade;
+};
+
+	struct student s1 = {14, "¾ûµ¢ÀÌ", 4.3};
+	
+	s.ID = 123456;
+	strcpy(s.name, "Juyeop");
+	s.grade = 4.2;
+	
+	printf("ID : %d\n", s.ID);
+	printf("name : %s\n", s.name);
+	printf("grade : %f\n", s.grade);
+
+
+}
