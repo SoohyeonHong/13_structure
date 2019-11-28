d9bafd3d49c3d908cfb6bbf32ecbd1c6771f0e73
SoohyeonHong <tngus4411@naver.com>
2019-11-28 16:06:43 +0900

    02

--- main.c
+++ main.c
@@ -3,7 +3,7 @@
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
-
+/*
 #01
 	
 struct student{
@@ -23,6 +23,82 @@ void main(void){
 	printf("name : %s\n", s1.name);
 	printf("grade : %f\n", s1.grade);
 
+*/
 
+int is_whitespace(char c)
+{
+	if( c == ' '  ||
+		c == '\n' ||
+		c == '\r' ||
+		c == '\t' ||
+		c == '('  )
+	{
+		return 1; //맞다 
+	}
+	
+	return 0; //아니다 
+}
+
+int fget_word(FILE* fp, char *word)
+{
+	int cnt;
+	char c;
+	
+	while ((c = fgetc(fp)) != EOF)
+	{
+		if (is_whitespace(c) == 0) //공백글자인지를 보는 것 
+			break;
+	}
+	if ( c == EOF )
+	{
+		return 0;
+	}
+	
+	cnt = 0;
+	word[cnt++] = c;
+	word[cnt] = '\0';
+	
+	while ((word[cnt] = fgetc(fp)) != EOF)
+	{
+		if (is_whitespace(word[cnt]) == 1)
+		{
+			word[cnt] = '\0';
+			break;
+		}
+		cnt++;
+	}
+	
+	return cnt;
+}
+
+#02
+int main(void){
+ 
+	FILE *fp;
+	char filepath[100];		//사용자가 입력한 문자열 저장 
+	char word[100];
+	
+	//file open
+	printf("input the file path : ");
+	scanf("%s", filepath, "r"); //scanf로 파일경로를 입력받음 
+	fp = fopen(filepath, "r");
+	
+	if (fp == NULL)
+	{
+		printf("file path is wrong! %s\n", filepath);
+		return -1; //프로그램 끝내버림 
+	}
+	
+	
+	//word reading & analysis
+	while (fget_word(fp, word) != 0)
+	{
+		printf("%s\n", word);
+	}
+	
+	fclose(fp);
+	//output
+	
+	return 0;
 }
 
